d45d3b47c40b48fa65026c00563a313cc7a40bf8
Joshua Onyeuche <joshuaonyeuche@gmail.com>
2022-08-05 23:05:33 +0100

    Header

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Layout({ title, children }) {
   return (
@@ -10,8 +11,18 @@ export default function Layout({ title, children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='flex'>
-        <header>Header</header>
+      <div className='flex flex-col min-h-screen justify-between'>
+        <header>
+          <nav className="flex h-12 justify-between">
+            <Link href="/">
+              <a className="text-lg font-bold">eCommX</a>
+            </Link>
+            <div>
+              <Link href="/cart">Cart</Link>
+              <Link href="/login">Login</Link>
+            </div>
+          </nav>
+        </header>
         <main>{children}</main>
         <footer>Footer</footer>
       </div>
