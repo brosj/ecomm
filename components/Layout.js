965bc51baebe2a3bd450eb1af9fdb2d839636518
Joshua Onyeuche <joshuaonyeuche@gmail.com>
2022-08-05 23:29:01 +0100

    Header

--- components/Layout.js
+++ components/Layout.js
@@ -13,13 +13,13 @@ export default function Layout({ title, children }) {
 
       <div className='flex flex-col min-h-screen justify-between'>
         <header>
-          <nav className="flex h-12 justify-between">
+          <nav className="flex h-12 shadow-md px-4 items-center justify-between">
             <Link href="/">
               <a className="text-lg font-bold">eCommX</a>
             </Link>
             <div>
-              <Link href="/cart">Cart</Link>
-              <Link href="/login">Login</Link>
+              <Link href="/cart"><a className="p-2">Cart</a></Link>
+              <Link href="/login"><a className="p-2">Login</a></Link>
             </div>
           </nav>
         </header>
