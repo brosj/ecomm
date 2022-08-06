3b1acb60eb40b44781c0564e3249836cb5193bb0
Joshua Onyeuche <joshuaonyeuche@gmail.com>
2022-08-06 01:02:28 +0100

    Adding and rendering products

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Layout from "../components/Layout";
+import data from "../utils/data";
+import ProductItem from "../components/ProductItem";
 
 export default function Home() {
   return (
     <Layout title="Home">
-      Main body
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.slug} product={product} />
+        ))}
+      </div>
     </Layout>
   )
 }
