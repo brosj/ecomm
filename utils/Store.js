6826845502dababf43b5ccc6ceb56dfaa3100026
Joshua Onyeuche <joshuaonyeuche@gmail.com>
2022-08-06 16:53:27 +0100

    Add to cart functionality

--- utils/Store.js
+++ utils/Store.js
@@ -12,9 +12,7 @@ const reducer = (state, action) => {
     case 'ADD_TO_CART': {
       const newItem = action.payload;
       const existItem = currentCartItems.find(item => item.slug === newItem.slug);
-      // const cartItems = existItems ? currentCartItems : [...currentCartItems, newItem];
       const cartItems = existItem ? currentCartItems.map(item => item.name === existItem.name ? newItem : item) : [...currentCartItems, newItem];
-      // return { ...state, cart: { cartItems } };
       return { ...state, cart: { ...state.cart, cartItems } };
     }
     default:
